82cba11a959b2f2a7be310d1a45ce6f047f1fea8
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-11 16:57:42 -0300

    feat: add Button on list components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import App from './App';
 import ThemeButton from './ThemeButton';
 import RenderIf from './RenderIf';
 import Header from './Header';
+import Button from './Button';
 
 export {
   App,
   ThemeButton,
   RenderIf,
   Header,
+  Button,
 };
