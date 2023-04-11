41f92136f059f8010057ea1730e9dec8f626650b
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-11 18:56:00 -0300

    feat: add PageHeader and SvgElipse on list components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,8 @@ import RenderIf from './RenderIf';
 import Header from './Header';
 import Button from './Button';
 import Footer from './Footer';
+import SvgElipse from './SvgElipse';
+import PageHeader from './PageHeader';
 
 export {
   App,
@@ -12,4 +14,6 @@ export {
   Header,
   Button,
   Footer,
+  SvgElipse,
+  PageHeader,
 };
