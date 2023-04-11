e86846f724b95167f29f3e72f5c3ee8a57a88c61
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-11 16:44:15 -0300

    feat: add Header on list of components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import App from './App';
 import ThemeButton from './ThemeButton';
 import RenderIf from './RenderIf';
+import Header from './Header';
 
 export {
   App,
   ThemeButton,
   RenderIf,
+  Header,
 };
