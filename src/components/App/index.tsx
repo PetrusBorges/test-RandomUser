ed40d9fd9b11e5169c804ce212f9eefccd512276
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-11 16:13:24 -0300

    feat: add Global styles and ThemButton at App component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,26 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../../assets/styles/global';
+
+import { ThemeButton } from '../../components/index';
+
+import useChangeTheme from '../../hooks/useChangeTheme';
+
 const App = () => {
+  const {
+    theme,
+    currentTheme,
+    handleToggleTheme,
+  } = useChangeTheme();
+
   return (
-    <>
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyles />
+      <ThemeButton
+        selectedTheme={theme}
+        onClick={handleToggleTheme}
+      />
       <h1>opa</h1>
-    </>
+    </ThemeProvider>
   );
 };
 
