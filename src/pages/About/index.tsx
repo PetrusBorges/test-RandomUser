15f426e231a83042f03dc8e0fc6d98fecdb6b83e
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-04-12 12:21:18 -0300

    feat: add About page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,17 @@
+import PageHeaderAbout from '../../assets/icons/pageHeaderAbout.svg';
+
+import { PageHeader, AboutInfo } from '../../components';
+
 const About = () => {
   return (
     <>
-      <h1>About</h1>
+      <PageHeader
+        logoHeader={PageHeaderAbout}
+        title='Informações'
+        description='Veja um pouco mais sobre a página!'
+      />
+
+      <AboutInfo />
     </>
   );
 };
